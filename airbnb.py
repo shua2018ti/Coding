96d94b086c78780d58f3c30a3e25d5bff8c62fd1
veganafro <jmuhia80@gmail.com>
2017-09-01 11:42:36 -0400

    add airbnb solutions

--- airbnb.py
+++ airbnb.py
@@ -4,6 +4,10 @@ def paginate(num, results):
     added_hosts = set()
     pending = []
     for item in results:
+        if len(pending) > 0:
+            if pending[0][0] not in added_hosts:
+                added_hosts.add(pending[0][0])
+                tmp_page.append(pending.pop(0))
         if len(tmp_page) < num:
             if item[0] not in added_hosts:
                 tmp_page.append(item)
@@ -11,16 +15,6 @@ def paginate(num, results):
             else:
                 pending.append(item)
         else:
-            tmp_page.append("")
-            for entry in tmp_page:
-                final_page.append(entry)
-            tmp_page = []
-            added_hosts = set()
-            for index, entry in enumerate(pending):
-                if len(tmp_page) < num:
-                    if entry[0] not in added_hosts:
-                        tmp_page.append(pending.pop(index))
-                        added_hosts.add(entry[0])
             tmp_page.append("")
             for entry in tmp_page:
                 final_page.append(entry)
