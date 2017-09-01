2ac9412a68777e36dafad3eb47ec3ad966e4f0bb
veganafro <jmuhia80@gmail.com>
2017-09-01 16:29:17 -0400

    clean up airbnb solution

--- airbnb.py
+++ airbnb.py
@@ -1,24 +1,83 @@
+"""
+Description:
+When a user submis a search query on our platform, we return a list of results
+scored according to the specified preferences. The search results are then presented
+over several web pages. Given multple listings can belong to the same host though,
+we want o be mindful that a host does not dominate the results in a single page.
+
+Problem:
+You are given an array of CSV strings representing search results. Each entry is
+composed of host_id, listing_id, score and city
+"""
+
 def paginate(num, results):
     tmp_page = []
     final_page = []
     added_hosts = set()
     pending = []
     for item in results:
-        if len(pending) > 0:
-            if pending[0][0] not in added_hosts:
-                added_hosts.add(pending[0][0])
-                tmp_page.append(pending.pop(0))
-        if len(tmp_page) < num:
-            if item[0] not in added_hosts:
+        clean_pending(pending, added_hosts, tmp_page, num)
+        if item.split(",")[0] not in added_hosts:
+            if len(tmp_page) < num:
+                added_hosts.add(item.split(",")[0])
                 tmp_page.append(item)
-                added_hosts.add(item[0])
             else:
-                pending.append(item)
+                tmp_page.append("")
+                for entry in tmp_page:
+                    final_page.append(entry)
+                tmp_page = []
+                added_hosts = set()
+                clean_pending(pending, added_hosts, tmp_page, num)
+                tmp_page.append(item)
+                added_hosts.add(item.split(",")[0])
         else:
-            tmp_page.append("")
-            for entry in tmp_page:
-                final_page.append(entry)
-            tmp_page = []
-            added_hosts = set()
-    for item in pending:
-        final_page.append(item)
+            pending.append(item)
+    counter = 0
+    while len(tmp_page) < num:
+        tmp_page.append(pending.pop(0))
+    tmp_page.append("")
+    while len(tmp_page) > 0:
+        final_page.append(tmp_page.pop(0))
+    while len(pending) > 0:
+        final_page.append(pending.pop(0))
+        if counter % 5 == 0:
+            final_page.append("")
+            counter = 0
+        counter += 1
+    return final_page
+
+def clean_pending(pending, added_hosts, tmp_page, num):
+    if len(pending) > 0:
+        counter = 0
+        length = len(pending)
+        while counter < length:
+            if pending[0].split(",")[0] not in added_hosts and len(tmp_page) < num:
+                added_hosts.add(pending[0].split(",")[0])
+                tmp_page.append(pending.pop(0))
+            counter += 1
+
+results_per_page = 5
+results = [
+    "1,28,300.6,San Francisco",
+    "4,5,209.1,San Francisco",
+    "20,7,203.4,Oakland",
+    "6,8,202.9,San Francisco",
+    "6,10,199.8,San Francisco",
+    "1,16,190.5,San Francisco",
+    "6,29,185.3,San Francisco",
+    "7,20,180.0,Oakland",
+    "6,21,162.2,San Francisco",
+    "2,18,161.7,San Jose",
+    "2,30,149.8,San Jose",
+    "3,76,146.7,San Francisco",
+    "2,14,141.8,San Jose"
+]
+
+print("Input\n")
+for item in results:
+    print(item)
+
+print("\nOutput\n")
+output = paginate(results_per_page, results)
+for item in output:
+    print(item)
--- palantir.py
+++ palantir.py
@@ -1,15 +1,18 @@
 """
-I recently interviewed at Palantir and this was one of the questions I came across following the Hackerrank challenge.
+I recently interviewed at Palantir and this was one of the questions I came
+across following the Hackerrank challenge.
 
 Implement a VList class. The behavior of the class is detailed below.
 
-A VList class is an object that contains linked arrays. Each array has a permissible capacity (beginning with 1). When
-the number of elements in the array reach the capacity, a new link in the VList is added to the tail with doubled capacity.
+A VList class is an object that contains linked arrays. Each array has a permissible
+capacity (beginning with 1). When the number of elements in the array reach the capacity,
+a new link in the VList is added to the tail with doubled capacity.
 
 [0] <-> [1, 2] <-> [3, 4, _, _] <--- tail
 
-In the example above, when the first link is filled with 1 element, another link in the VList is added with capacity 2. After
-the number of elements in this link reach capacity, another link is added with capacity 4.
+In the example above, when the first link is filled with 1 element, another link
+in the VList is added with capacity 2. After the number of elements in this link
+reach capacity, another link is added with capacity 4.
 
 Implement the add_to_list(element) method that adds element to the tail of the VList
 Implement the at_index(index) method that returns the value store in the VList at index
