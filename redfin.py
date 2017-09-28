65a480bda9c3fb4245a4ddb260308270710ac51b
veganafro <jmuhia80@gmail.com>
2017-09-28 00:10:07 -0400

    finish redfin

--- redfin.py
+++ redfin.py
@@ -4,4 +4,16 @@ of rooms.
 """
 
 def schedule_meetings(meetings):
-    meetings_sorted = sorted(meetings, lambda x: (x[0], x[1]))
+    meetings_sorted = sorted(meetings, key=lambda x: (x[0], x[1]))
+    rooms = []
+    for meeting in meetings_sorted:
+        added = False
+        for room in rooms:
+            if meeting[0] >= room[-1][1]:
+                room.append(meeting)
+                added = True
+        if not added:
+            rooms.append([meeting])
+    return (len(rooms), rooms)
+
+print(schedule_meetings([(9, 14), (9, 10), (11, 14), (7, 9), (13, 15)]))
