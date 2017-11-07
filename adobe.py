019a316e9288507642c54b355729b0a9c1cc6b5c
jeremymuhia <jmuhia80@gmail.com>
2017-11-07 00:01:55 -0500

    finish adobe

--- adobe.py
+++ adobe.py
@@ -8,4 +8,17 @@ nums = [2, 3, 4, 5, 2] # products = [120, 80, 60, 48, 128]
 def get_products(nums):
     prod_so_far = 1
     left = []
-    for idx, num in enumerate(nums):
+    for num in nums:
+        left.append(prod_so_far)
+        prod_so_far *= num
+    prod_so_far = 1
+    right = []
+    for idx in range(len(nums) - 1, -1, -1):
+        right.insert(0, prod_so_far)
+        prod_so_far *= nums[idx]
+    products = []
+    for num1, num2 in zip(left, right):
+        products.append(num1 * num2)
+    return products
+
+print(get_products(nums))
