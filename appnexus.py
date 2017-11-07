6efaf3e40dcfecd45f595661c533dece3863f46f
jeremymuhia <jmuhia80@gmail.com>
2017-11-06 23:02:26 -0500

    start appnexus

---
+++ appnexus.py
@@ -0,0 +1,12 @@
+"""
+Given an dictionary containing students as keys and an array of students that should come before the
+key, flatten this data structure
+
+{
+    'A':['B', 'C', 'D'],
+    'B':['C', 'D'],
+    'C':['D', 'F'],
+}
+
+[D, F, C, B, A]
+"""
