18e2d26d23802e218f8d6b3d86e4655a5da2e506
jeremymuhia <jmuhia80@gmail.com>
2017-11-06 23:49:17 -0500

    finish arrange students

--- appnexus.py
+++ appnexus.py
@@ -8,5 +8,36 @@ key, flatten this data structure
     'C':['D', 'F'],
 }
 
-[D, F, C, B, A]
+[D, F, C, B, A] <-- front of line
 """
+
+students = {
+    'A':set(['B', 'C', 'D']),
+    'B':set(['C', 'D']),
+    'C':set(['D', 'F']),
+}
+
+
+def make_comparator(less_than):
+    def compare(x, y):
+        if less_than(x, y):
+            return -1
+        elif less_than(y, x):
+            return 1
+        return 0
+    return compare
+
+def compare_students(student_1, student_2):
+    if students.get(student_1) is not None and student_2 in students[student_1]:
+        return True
+    return False
+
+def arrange_students(students):
+    uniques = set()
+    for key, value in students.items():
+        uniques.add(key)
+        for item in value:
+            uniques.add(item)
+    return sorted(list(uniques), cmp=make_comparator(compare_students))
+
+print(arrange_students(students))
